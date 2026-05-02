f251ac7ce0f3ec2a23b49a73b3811f190dc5ee5e
felipechurio <felipe.churio@davinci.edu.ar>
2026-05-02 03:58:22 -0300

    InProgress

--- Assets/Scripts/DoorFix/DoorFixer.cs
+++ Assets/Scripts/DoorFix/DoorFixer.cs
@@ -1,14 +1,26 @@
+﻿using System.Collections;
 using UnityEngine;
 
 public class DoorFixer : MonoBehaviour
 {
+    private HingeJoint _joint;
+
+    [SerializeField] float SpringAmount;
+
+    [SerializeField] float LerpDuration;
+
+    private JointSpring SpringValue;
+
+    bool Flag = false;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            JointSpring SpringValue = _joint.spring;
-            SpringValue.spring = 0f;
+           // _joint.useSpring = false;
+
+            SpringValue = _joint.spring;
+           // SpringValue.spring = SpringAmount*0;
             _joint.spring = SpringValue;
 
         }
@@ -16,24 +28,71 @@ public class DoorFixer : MonoBehaviour
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
-        {
-            JointSpring SpringValue = _joint.spring;
-            SpringValue.spring = 80f;
-            _joint.spring = SpringValue;
+        //if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+       // {
+            
 
-        }
+       // }
     }
-    private HingeJoint _joint;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
        _joint = GetComponent<HingeJoint>(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float angulo = Mathf.Abs(_joint.angle);
+
+        if (angulo < 20f)  // 20 grados del centro
+        {
+            if (Flag == true)
+            {
+               // StartCoroutine(SpringLerp());
+               // StartCoroutine(DamperLerp());
+            }
+            Flag = false;
+           
+        }
+
+        if (angulo > 20f)
+        {
+            StopAllCoroutines();
+            Flag = true;
+        }
+
+    }
+
+    private IEnumerator SpringLerp()
+    {
+        for (float t = 0; t < 1; t += Time.deltaTime/LerpDuration)
+        {
+            _joint.useSpring = true;
+            float resultado = Mathf.Lerp(0, 1000f, t);
+            SpringAmount = resultado;
+            SpringValue.spring = resultado;
+            yield return null;
+        }
+    }
+
+    private IEnumerator DamperLerp()
+    {
+        for (float t = 0; t < 1; t += Time.deltaTime / LerpDuration)
+        {
+           // _joint.useSpring = false;
+            float resultado = Mathf.Lerp(0, 300f, t);
+            SpringValue.damper = resultado;
+            _joint.spring = SpringValue; 
+            yield return null;
+        }
+    }
+
+    public void StopDoor()
+    {
+        _joint.useSpring = true;
+        StartCoroutine(SpringLerp());
+        SpringValue = _joint.spring;
+        SpringValue.spring = SpringAmount;
+        _joint.spring = SpringValue;
     }
 }
+
