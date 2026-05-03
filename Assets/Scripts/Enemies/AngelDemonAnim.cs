9707a1f9ad9a3d83b0d7c9b37df07bdcbb133355
gsalvatierra-dv <gabriel.salvatierra@davinci.edu.ar>
2026-05-03 20:06:49 -0300

    asd

---
+++ Assets/Scripts/Enemies/AngelDemonAnim.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class AngelDemonAnim : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+ 
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void WalkAnim(float speed)
+    {
+        /*animator.SetBool("IsAttacking", false);
+        animator.SetBool("IsHeadshot", false);
+        animator.SetBool("IsDead", false);*/
+    }
+
+    public void AttackAnim()
+    {
+        animator.SetBool("IsAttacking", true);
+        animator.SetBool("IsHeadshot", false);
+        animator.SetBool("IsDead", false);
+    }
+
+    public void HeadshotAnim()
+    {
+        animator.SetTrigger("Headshot");
+    }
+
+    public void DieAnim()
+    {
+        animator.SetBool("IsAttacking", true);
+    }
+
+}
