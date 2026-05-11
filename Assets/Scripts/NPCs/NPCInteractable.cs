a3d800b09aac42181f1ac44929d94d69bb92e26a
gsalvatierra-dv <gabriel.salvatierra@davinci.edu.ar>
2026-05-10 23:42:38 -0300

    Animations setup

--- Assets/Scripts/NPCs/NPCInteractable.cs
+++ Assets/Scripts/NPCs/NPCInteractable.cs
@@ -2,18 +2,27 @@ using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour, IInteractable
 {
+    [SerializeField] private string _wavingTrigger = "Waving";
 
-  public void Interact(Transform interactorTransform)
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Interact(Transform interactorTransform)
     {
         Debug.Log("Interaction!");
+
+        _animator.SetTrigger(_wavingTrigger);
 
         // Give Bullets
 
         // Hide Interact UI
 
-        // Animation
-
         // VFX
+
     }
 
 }
