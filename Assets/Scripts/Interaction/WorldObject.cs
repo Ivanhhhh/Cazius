4e0a410dbe4c62a0975d0221bd68334ecdce8455
Delfuss <delfina.gimenez@davinci.edu.ar>
2026-05-14 10:14:42 -0300

    Activate/Desactivate Objects Scenes

---
+++ Assets/Scripts/Interaction/WorldObject.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class WorldObject : MonoBehaviour // Va en cada objecto que se quiera prender/desactivar por escena 
+{
+    [Header("Worlds")]
+    [SerializeField] private bool _showInEden;
+    [SerializeField] private bool _showInPurgatory;
+
+    [SerializeField] private GameObject _visualRoot;
+
+    void Start()
+    {
+        UpdateWorldVisibility();
+    }
+    void OnEnable()
+    {
+        WorldChangeManager.Instance.SwapToEdenEvent += HandleSwapToEden;
+        WorldChangeManager.Instance.SwapToPurgatoryEvent += HandleSwapToPurgatory;
+    }
+
+    void OnDisable()
+    {
+        if (WorldChangeManager.Instance == null) return;
+
+        WorldChangeManager.Instance.SwapToEdenEvent -= HandleSwapToEden;
+        WorldChangeManager.Instance.SwapToPurgatoryEvent -= HandleSwapToPurgatory;
+    }
+
+    private void HandleSwapToEden()
+    {
+        _visualRoot.SetActive(_showInEden);
+    }
+
+    private void HandleSwapToPurgatory()
+    {
+        _visualRoot.SetActive(_showInPurgatory);
+    }
+
+    private void UpdateWorldVisibility()
+    {
+        bool isInEden = WorldChangeManager.Instance.IsInEden;
+
+        if (isInEden) _visualRoot.SetActive(_showInEden);
+        else _visualRoot.SetActive(_showInPurgatory);
+    }
+}
