512a77296c3b4b7abf959952bb5e4e895d2f7aac
felipechurio <felipe.churio@davinci.edu.ar>
2026-05-24 18:11:13 -0300

    d

--- Assets/Scripts/PauseMenu/Options/ChangeOption.cs
+++ Assets/Scripts/PauseMenu/Options/ChangeOption.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class ChangeOption : MonoBehaviour
 {
-   private (Button last, Button current) tupleButtons;
-   private  CloseOptions closeOptions;
+   private (Button last, Button current) tupleButtons; // una tupla para que al apretar un boton se desactiva el canvas del boton anterior y se activa el del que aprete recien (se activa el de current y el de last se desactiva)
+   private  CloseOptions closeOptions; // referencia a la clase que a alpretar un boton el mismo s epone en current de la tupla.
 
-    private Canvas _canvas1;
+    private Canvas _canvas1; // variable donde se almacenan el boton current y el last (se mantiene la referencia).
     private Canvas _canvas2;
 
     private void Start()
@@ -36,11 +36,8 @@ public class ChangeOption : MonoBehaviour
             tupleButtons.last = tupleButtons.current; // el ultimo boton es el ultimo que aprete
             tupleButtons.current = PressedButton; // el boton actual es el que apreto ahora
 
-            //Debug.Log("Pressed: " + PressedButton.name);
-            //Debug.Log("Canvas del pressed: " + PressedButton.GetComponentInChildren<Canvas>());
-
             _canvas2 = tupleButtons.last.GetComponentInChildren<Canvas>();
-               _canvas2.enabled = false;
+            _canvas2.enabled = false;
             
 
             _canvas1 = tupleButtons.current.GetComponentInChildren<Canvas>();
