da4b1a6b37cfc4d483c82412d53865d92369e341
Ivanhhhh <ivan.solari@davinci.edu.ar>
2026-05-31 20:18:22 -0300

    UI Visual Changes

--- Assets/Scripts/Inventory/CraftManager.cs
+++ Assets/Scripts/Inventory/CraftManager.cs
@@ -5,7 +5,10 @@ public class CraftManager : MonoBehaviour
     [Header("Listas y Botones")]
     [SerializeField] GameObject[] _objectsList;
     [SerializeField] GameObject[] _buttonsToPush;
-     
+    [SerializeField] CraftTabButton[] _buttonsVisuals;
+
+    private int _currentIndex = 0;
+
     void Start()
     {
         ActivateList(0);
@@ -13,9 +16,21 @@ public class CraftManager : MonoBehaviour
      
     public void ActivateList(int indexToActivate)
     {
+        _currentIndex = indexToActivate;
+
         for (int i = 0; i < _objectsList.Length; i++)
         {
             _objectsList[i].SetActive(i == indexToActivate); 
+        }
+
+        UpdateButtonsVisuals();
+    }
+
+    private void UpdateButtonsVisuals()
+    {
+        for (int i = 0; i < _buttonsVisuals.Length; i++)
+        {
+            _buttonsVisuals[i].SetSelectedButton(i == _currentIndex);
         }
     }
 }
