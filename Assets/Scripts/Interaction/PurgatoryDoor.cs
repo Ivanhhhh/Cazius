e23cefb51b42846cdb97a29f7ba170cdca98cebd
Delfuss <delfina.gimenez@davinci.edu.ar>
2026-06-13 12:30:32 -0300

    Door Open

--- Assets/Scripts/Interaction/PurgatoryDoor.cs
+++ Assets/Scripts/Interaction/PurgatoryDoor.cs
@@ -8,7 +8,8 @@ public class PurgatoryDoor : MonoBehaviour, IEInteractable
     [SerializeField] private float _openAngle;
     [SerializeField] private string _interactText = "You need Purgatory Key";
 
-    [SerializeField] private Key _key;
+    [Header("Open Door Anim")]
+    [SerializeField] private WhenOpenDoor _whenOpenDoor;
     [SerializeField] private PlayerMovement _playerMovement;
     [SerializeField] private Animator _playerAnimator;
 
@@ -26,7 +27,7 @@ public class PurgatoryDoor : MonoBehaviour, IEInteractable
         _playerMovement.enabled = false;
         _playerAnimator.speed = 0f;
 
-        yield return StartCoroutine(_key.WhenKeyOpenDoor(transform, "KeyOpen"));
+        yield return StartCoroutine(_whenOpenDoor.WhenKeyOpenDoor("PurgatoryDoor"));
 
         transform.Rotate(0, _openAngle, 0);
         _isOpen = true;
