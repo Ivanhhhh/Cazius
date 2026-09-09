0b952a4461112ff8b3e8dd547e63c18876972ce8
felipechurio <felipe.churio@davinci.edu.ar>
2026-09-09 00:39:08 -0300

    InProgress

---
+++ Assets/Scripts/SoulEnergyManager/SoulEnergyManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System;
+
+public class SoulEnergyManager : MonoBehaviour
+{
+    public static SoulEnergyManager Instance {get; private set;}
+
+     [SerializeField] private long _maxSoulEnergyValue;
+      
+    public long MaxSoulEnergyValue
+    {
+        get
+        {
+          return _maxSoulEnergyValue;
+        }
+        private set
+        {
+          _maxSoulEnergyValue = value;
+        }
+    }
+
+private long _currentSoulEnergy;
+public long CurrentSoulEnergy
+{
+    get
+    { 
+      return _currentSoulEnergy;
+    }
+
+    private set
+    {
+        _currentSoulEnergy = value < 0 ? 0 : value;
+    }
+}
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) Instance = null;
+    }
+
+    public void AddSoulEnergy(long AmountToAdd)
+    {   if (AmountToAdd <= 0) return;
+        else
+        {
+          var Plus = CurrentSoulEnergy + AmountToAdd;
+          var Add = Plus > MaxSoulEnergyValue? Plus = MaxSoulEnergyValue : Plus = CurrentSoulEnergy + AmountToAdd;
+          CurrentSoulEnergy = Add;
+        }
+        
+    }
+
+    public void RemoveSoulEnergy(long AmountToRemove)
+    {
+      if (AmountToRemove <= 0) return;
+      else
+      {
+        var Change = CurrentSoulEnergy >= AmountToRemove? CurrentSoulEnergy = CurrentSoulEnergy - AmountToRemove: CurrentSoulEnergy = CurrentSoulEnergy;
+        CurrentSoulEnergy = Change;
+      }
+    }
+}
